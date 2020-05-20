76254b3c50939d37ff9ec5a93b9510db805e98d3
Sylvia Soto <sylviadiane69@gmail.com>
2020-05-20 17:46:39 -0500

    changes to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -11,10 +11,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-// // Connect to the Mongo DB
-// mongoose.connect(MONGODB_URI);
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
  { useNewUrlParser: true,
   useFindAndModify: false
