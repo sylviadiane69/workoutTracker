f6d52a02a53cb4b43303d07d19c7f180641bd5a6
Sylvia Soto <sylviadiane69@gmail.com>
2020-05-20 17:58:10 -0500

    changes to server.js file

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,17 +1,17 @@
 const db = require("../models ")
 module.exports = (app) => {
     //   Workout Routes   //
-    app.get("/api/workouts", (req, res) => {
-        db.workout.find({}, (err, workouts) => {
+    app.get("/api/workout", (req, res) => {
+        db.workout.find({}, (err, workout) => {
             if (err) {
                 console.log(err);
             } else {
-                res.json(workouts)
+                res.json(workout)
             }
         });
     });
     //add excerise, set id, push to model, set true
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
+    app.put("/api/workout/:workout", ({ params, body }, res) => {
         db.workout.findOneAndUpdate({ _id: params.id },
             { $push: { excercises: body } },
             { upsert: true, useFindandModify: false },
@@ -20,7 +20,7 @@ module.exports = (app) => {
             })
     });
     //create new workout
-    app.post("/api/workouts", (req, res) => {
+    app.post("/api/workout", (req, res) => {
         db.workout.create({}).then(newWorkout => {
             res.json(newWorkout);
         });
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
